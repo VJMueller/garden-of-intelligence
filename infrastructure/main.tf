407b5693e950349a0d2cc7733e4f6d1ac690864b
VJMueller <vemueller@babbel.com>
2023-06-28 09:34:09 +0200

    Add vpc security group and vpc connector

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,13 +8,28 @@ resource "aws_apprunner_service" "garden_of_intelligence" {
     }
     image_repository {
       image_configuration {
-        port = "3000" #The port that your application listens to in the container                             
+        port = "3000" #The port that your application listens to in the container
+        runtime_environment_variables = {
+          ENVIRONMENT = "production"
+          RAILS_ENV = "production"
+        }                       
       }
       image_identifier = "${aws_ecr_repository.garden-of-intelligence.repository_url}:latest"                          
       image_repository_type = "ECR"
     }
-
   }
+  network_configuration {
+    egress_configuration {
+      egress_type       = "VPC"
+      vpc_connector_arn = aws_apprunner_vpc_connector.connector.arn
+    }
+  }
+}
+
+resource "aws_apprunner_vpc_connector" "connector" {
+  vpc_connector_name = "garden_of_intelligence_vpc_connector"
+  subnets            = aws_subnet.private[*].id
+  security_groups    = [aws_security_group.db-sg.id]
 }
 
 output "service_url" {
