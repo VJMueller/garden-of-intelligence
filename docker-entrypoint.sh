da7e9dc95b6ce288ea7188c5cea834fa8b49c359
VJMueller <vemueller@babbel.com>
2023-10-07 00:10:22 +0200

    Remove comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,17 +3,10 @@
 # Exit immediately if any command returns a non-zero status code
 set -e
 
-#echo "Running db:create"
-#bundle exec rake db:create
-
-#echo "Running db:schema:load"
-#bundle exec rake db:schema:load
-
 echo "Running db:migrate"
 bundle exec rake db:migrate
 
 echo "Running db:seed"
 bundle exec rake db:seed
 
-# Start the Rails server
 bundle exec rails server -b "0.0.0.0" -p 3000
