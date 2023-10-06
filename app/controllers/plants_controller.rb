a0138a989ef1f279e4d39e2fc7f505ad607ecdda
VJMueller <vemueller@babbel.com>
2023-10-06 20:02:43 +0200

    Add more db details to the view

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,8 +1,17 @@
 class PlantsController < ApplicationController
   def index
     begin
-      ActiveRecord::Base.connection.execute('SELECT 1')
+      ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
       @connection_success = true
+
+      connection = ActiveRecord::Base.connection
+      @adapter = connection.adapter_name
+      @database_name = connection.current_database
+      @database_version = connection.raw_connection.server_version 
+      @host = connection.host
+      @port = connection.port
+      @username = connection.raw_connection.user
+      @current_user = connection.raw_connection.username 
     rescue ActiveRecord::NoDatabaseError, ActiveRecord::StatementInvalid => e
       @connection_success = false
       @error_message = e.message
