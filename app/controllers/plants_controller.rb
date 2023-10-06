27271b6d2d588288eb76fed9f6cc5bb4c63e4ed5
Verena Müller <66951423+VJMueller@users.noreply.github.com>
2023-10-07 00:11:13 +0200

    Add docker entrypoint (#15)

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
   def index
-    @plants = "There will be list of plans soon"
+    @plants = Plant.all
   end
 end
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,12 @@
-#!/usr/bin/env sh
-set -ex
+#!/bin/sh
 
-exec "$@"
+# Exit immediately if any command returns a non-zero status code
+set -e
+
+echo "Running db:migrate"
+bundle exec rake db:migrate
+
+echo "Running db:seed"
+bundle exec rake db:seed
+
+bundle exec rails server -b "0.0.0.0" -p 3000
