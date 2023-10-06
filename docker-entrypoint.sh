98ad26246d8dab66dc238f30326cfaa49277e945
VJMueller <vemueller@babbel.com>
2023-10-06 23:39:49 +0200

    remove db:creation and seed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 # Exit immediately if any command returns a non-zero status code
 set -e
 
-echo "Running db:create"
-bundle exec rake db:create
+#echo "Running db:create"
+#bundle exec rake db:create
 
 echo "Running db:schema:load"
 bundle exec rake db:schema:load
@@ -12,8 +12,8 @@ bundle exec rake db:schema:load
 echo "Running db:migrate"
 bundle exec rake db:migrate
 
-echo "Running db:seed"
-bundle exec rake db:seed
+#echo "Running db:seed"
+#bundle exec rake db:seed
 
 # Start the Rails server
 bundle exec rails server -b "0.0.0.0" -p 3000
