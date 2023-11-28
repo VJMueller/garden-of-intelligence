ed995e2d0cfad1380e77926016a9195c22fa0421
VJMueller <vemueller@babbel.com>
2023-11-28 14:32:23 +0100

    Add user controller, policy and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
+  resources :users, only: %i[index show edit update destroy]
   resources :plants
   get '/database_info', to: 'database_info#show'
 end
